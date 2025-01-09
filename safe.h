54b282af7ddfe738f8665b034cff53426d37e222
slaimon <simo.bertolucci@gmail.com>
2025-01-09 02:30:35 +0100

    Remove unused from safe

--- safe.h
+++ safe.h
@@ -28,14 +28,4 @@
 void* safe_malloc(size_t bytes);
 void* safe_realloc(void* ptr, size_t nmemb, size_t size);
 
-// restituisce la prima dirent leggibile, se ne trova una.
-// solleva un warning per ciascuna dirent illeggibile che incontra.
-// se non ne trova nessuna leggibile restituisce NULL
-struct dirent* safe_readdir(DIR* dirp, char* path);
-
-// terminano il processo se la creazione o l'inizializzazione falliscono
-pthread_t safe_pthreadCreate(void* (*start_fun) (void*), void* arg);
-void safe_pthreadMutexInit(pthread_mutex_t* mutex);
-void safe_pthreadCondInit(pthread_cond_t* cond);
-
 #endif
