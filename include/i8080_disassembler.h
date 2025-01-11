9e5b43202934e40d34fa2461221c1918a015bea2
slaimon <simo.bertolucci@gmail.com>
2025-01-11 01:50:29 +0100

    Remove unused macro, edit comments

--- include/i8080_disassembler.h
+++ include/i8080_disassembler.h
@@ -4,12 +4,11 @@
 #include "bytestream.h"
 #include "i8080_instruction.h"
 
-#define I8080_DISASSEMBLER_FAIL   -2  // retval on error
 #define I8080_DISASSEMBLER_LITTLE_ENDIAN  true
 
 
-// returns a struct containing the disassembly of the instruction found at the specified address in the memory 
-i8080_instruction_t disassemble_instruction( uint8_t* mem , unsigned int addr );
+// returns a struct describing the instruction found at the specified address in the memory 
+i8080_instruction_t disassemble_instruction(uint8_t* mem , unsigned int addr);
 
 // returns a string containing the text disassembly of the given program
 void disassemble_program(bytestream_t program, FILE* ofp);
