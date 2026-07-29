8e6ae62898d299f33d1210f1e267d971170c32eb
Simone Bertolucci <simo.bertolucci@gmail.com>
2026-07-29 20:00:49 +0200

    Refactor i8080_cpm

--- source/i8080_cpm.c
+++ source/i8080_cpm.c
@@ -1,50 +1,71 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include "i8080.h"
 
 const char separator[] = "\n\n--------------------------------------------------\n";
 
+// print character in register A
+static void print_char(const i8080_t* machine, FILE* ofp) {
+    fputc(machine->A, ofp);
+}
+
+// print string found at $DE and terminating with '$'
+static void print_string(const i8080_t* machine, FILE* ofp) {
+    uint16_t address = i8080_get_de(machine);
+    char c;
+    do {
+        c = machine->mem[address];
+        if (c != '$')
+            fputc(c, ofp);
+        ++address;
+    } while (c != '$');
+}
+
+static void fail_with_error(uint16_t svc, uint16_t address) {
+    fprintf(stderr, "\nERROR: unimplemented supervisor call: %04Xh\nFound at address: %04Xh\n", svc, address);
+    exit(EXIT_FAILURE);
+}
+
+// Acts in stead of the CP/M operating system using ofp as an output terminal
+static void handle_supervisor_call(const i8080_t* machine, FILE* ofp) {
+    uint16_t svc = machine->C;
+    uint16_t pc = machine->programCounter;
+    switch (svc) {
+        case 2:
+            print_char(machine, ofp);
+            break;
+
+        case 9:
+            print_string(machine, ofp);
+            break;
+
+        default:
+            fail_with_error(svc, pc);
+    }
+}
+
 bool handle_cpm_calls_file(const i8080_t* machine, FILE* ofp) {
     const uint16_t pc = machine->programCounter;
     
-    // HLT (terminate)
+    // terminate if we found a HLT instruction
     if (machine->mem[pc] == 0x76) {
         fputs(separator, ofp);
         fprintf(ofp, "HLT instruction encountered at %04Xh, terminating.\n", pc);
         return true;
     }
 
-    // JMP 00 (terminate)
-    if (pc == 0x00) {
-        fputs(separator, ofp);
-        fprintf(ofp, "User program returned control to CP/M, terminating.\n");
-        return true;
+    switch (pc) {
+        case 0x00:
+            // terminate if we found JMP 00
+            fputs(separator, ofp);
+            fprintf(ofp, "User program returned control to CP/M, terminating.\n");
+            return true;
+
+        case 0x05:
+        handle_supervisor_call(machine, ofp);
+            return false;
+        
+        default:
+            return false;
     }
-
-    // supervisor call
-    if (pc == 0x05) {
-        const uint16_t svc = machine->C;
-        switch (svc) {
-            case 2:
-                // print character in register A
-                fputc(machine->A, ofp);
-                break;
-            case 9:;
-                // print string found at $DE and terminating with '$'
-                uint16_t address = i8080_get_de(machine);
-                char c;
-                do {
-                    c = machine->mem[address];
-                    if (c != '$')
-                        fputc(c, ofp);
-                    ++address;
-                } while (c != '$');
-                break;
-
-            default:
-                fprintf(stderr, "\nERROR: unimplemented supervisor call: %04Xh\nFound at address: %04Xh\n", svc, pc);
-                exit(EXIT_FAILURE);
-        }
-    }
-
-    return false;
 }
